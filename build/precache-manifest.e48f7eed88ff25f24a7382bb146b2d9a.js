6a657c725eec7dd0b4073ba6272e6de2d6834fd7
Kelson <kelsoncarmozine@globo.com>
2020-03-06 15:05:25 -0300

    Final

---
+++ build/precache-manifest.e48f7eed88ff25f24a7382bb146b2d9a.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f8e7a5a229659cc9d4773214cff2070",
+    "url": "/index.html"
+  },
+  {
+    "revision": "180b1b9a9ebe0a7779d7",
+    "url": "/static/js/2.4d74b75b.chunk.js"
+  },
+  {
+    "revision": "b39be5351e9d0990cbd0",
+    "url": "/static/js/main.f7beaa16.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
